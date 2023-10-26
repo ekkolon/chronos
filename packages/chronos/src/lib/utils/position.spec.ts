6de396528382cbc4ae0ab8de01f61389248e9712
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2023-10-26 12:48:27 +0200

    feat(chronos): add common position utils
    Add a set of essential position utilities for the Chronos package,
    including functions for orientation, 2D area, and main axis position
    retrieval.

---
+++ packages/chronos/src/lib/utils/position.spec.ts
@@ -0,0 +1,143 @@
+import {
+  Area2d,
+  Orientation,
+  OrientationError,
+  clamp,
+  generateTranslate3dProperty,
+  getMainAxisPosition,
+  is3DTranslatable,
+  isArea2d,
+  isOrientation,
+} from './position';
+
+describe('utils:position:clamp', () => {
+  it('should return 0 if position is negative', () => {
+    expect(clamp(-5, 10)).toBe(0);
+  });
+
+  it('should return the position if it is within the distance range', () => {
+    expect(clamp(5, 10)).toBe(5);
+  });
+
+  it('should return the distance if position is greater than distance', () => {
+    expect(clamp(15, 10)).toBe(10);
+  });
+
+  it('should handle position equal to 0', () => {
+    expect(clamp(0, 10)).toBe(0);
+  });
+
+  it('should handle position equal to the distance', () => {
+    expect(clamp(10, 10)).toBe(10);
+  });
+});
+
+describe('utils:position:Orientation', () => {
+  it('should define valid orientation values', () => {
+    expect(Orientation.Vertical).toBe('vertical');
+    expect(Orientation.Horizontal).toBe('horizontal');
+  });
+
+  it('isOrientation should return true for valid orientations', () => {
+    expect(isOrientation(Orientation.Vertical)).toBe(true);
+    expect(isOrientation(Orientation.Horizontal)).toBe(true);
+  });
+
+  it('isOrientation should return false for invalid orientations', () => {
+    expect(isOrientation('invalid')).toBe(false);
+    expect(isOrientation(123)).toBe(false);
+    expect(isOrientation(null)).toBe(false);
+  });
+});
+
+describe('utils:position:isArea2d', () => {
+  it('should return true for valid 2D areas', () => {
+    const validArea: Area2d = { x: 10, y: 20 };
+    expect(isArea2d(validArea)).toBe(true);
+  });
+
+  it('should return false for invalid 2D areas', () => {
+    const invalidArea1 = { x: 10, z: 20 };
+    const invalidArea2 = { x: 'not a number', y: 20 };
+    expect(isArea2d(invalidArea1)).toBe(false);
+    expect(isArea2d(invalidArea2)).toBe(false);
+    expect(isArea2d(null)).toBe(false);
+  });
+});
+
+describe('utils:position:getMainAxisPosition', () => {
+  it('should return the y coordinate for a valid vertical orientation', () => {
+    const verticalPosition: Area2d = { x: 10, y: 20 };
+    const result = getMainAxisPosition(Orientation.Vertical, verticalPosition);
+    expect(result).toBe(verticalPosition.y);
+  });
+
+  it('should return the x coordinate for a valid horizontal orientation', () => {
+    const horizontalPosition: Area2d = { x: 10, y: 20 };
+    const result = getMainAxisPosition(Orientation.Horizontal, horizontalPosition);
+    expect(result).toBe(horizontalPosition.x);
+  });
+
+  it('should return the value for a valid integer position', () => {
+    const intValue = 42;
+    const result = getMainAxisPosition(Orientation.Vertical, intValue);
+    expect(result).toBe(intValue);
+  });
+
+  it('should throw an OrientationError for invalid orientation types', () => {
+    const invalidOrientation = 'invalid';
+    expect(() => getMainAxisPosition(invalidOrientation as never, 10)).toThrow(OrientationError);
+  });
+
+  it('should throw an OrientationError for invalid position values', () => {
+    const invalidPosition = 'not a number';
+    expect(() => getMainAxisPosition(Orientation.Vertical, invalidPosition as never)).toThrow(
+      OrientationError
+    );
+  });
+});
+
+describe('utils:position:is3DTranslatable', () => {
+  it('should return true for a valid 3D translation object', () => {
+    const validTranslation = { x: 10, y: 20, z: 30 };
+    expect(is3DTranslatable(validTranslation)).toBe(true);
+  });
+
+  it('should return true for an object with valid partial translation keys', () => {
+    const partialTranslation = { x: 10, y: 20 };
+    expect(is3DTranslatable(partialTranslation)).toBe(true);
+  });
+
+  it('should return false for an object with invalid keys', () => {
+    const invalidTranslation = { x: 10, y: 20, w: 30 };
+    expect(is3DTranslatable(invalidTranslation)).toBe(false);
+  });
+
+  it('should return false for null input', () => {
+    expect(is3DTranslatable(null)).toBe(false);
+  });
+
+  it('should return false for non-object input', () => {
+    expect(is3DTranslatable('not an object')).toBe(false);
+  });
+});
+
+describe('utils:position:generateTranslate3dProperty', () => {
+  it('should generate a valid translate3d property for a valid translation object', () => {
+    const validTranslation = { x: 10, y: 20, z: 30 };
+    expect(generateTranslate3dProperty(validTranslation)).toBe('translate3d(10px, 20px, 30px)');
+  });
+
+  it('should return "unset" for an invalid translation object', () => {
+    const invalidTranslation = { x: 10, y: 20, w: 30 };
+    expect(generateTranslate3dProperty(invalidTranslation)).toBe('unset');
+  });
+
+  it('should handle null input by returning "unset"', () => {
+    expect(generateTranslate3dProperty(null)).toBe('unset');
+  });
+
+  it('should handle non-object input by returning "unset"', () => {
+    expect(generateTranslate3dProperty('not an object')).toBe('unset');
+  });
+});
