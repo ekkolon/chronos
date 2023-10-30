26be0ad911df3a7e13b936b8d4417cba10a1c928
Nelson Dominguez <18242749+ekkolon@users.noreply.github.com>
2023-10-30 14:52:15 +0100

    feat(chronos): add `ChronTimelineLabel` directive

---
+++ packages/chronos/src/lib/components/timeline/timeline-label.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright Nelson Dominguez All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at the root of this project.
+ */
+
+import { Component, Input } from '@angular/core';
+
+import { PositionableElement } from '../../directives/positionable-element';
+
+/**
+ * A component representing a label element within a Chronos timeline.
+ * This component is used to display labels with text content that can be positioned
+ * based on an `Orientation` value and a position pixel value along the main axis
+ * determined by that orientation.
+ */
+@Component({
+  selector: 'chron-timeline-label',
+  template: `<div class="chron-timeline-label__text">{{ displayText }}</div>`,
+  standalone: true,
+  host: {
+    class: 'chron-timeline-label',
+  },
+})
+export class ChronTimelineLabel extends PositionableElement {
+  @Input({ required: true }) displayText!: string | null;
+}
